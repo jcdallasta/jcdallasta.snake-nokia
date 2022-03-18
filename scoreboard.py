2bd9307857553063a39a9509e78f21c5781c2a6a
Juan Cruz Dall'Asta <juancruz.da@gmail.com>
2022-03-18 10:47:56 +0100

    Update scoreboard.py
    In this new update, the high score gets stored in the data.txt file

--- scoreboard.py
+++ scoreboard.py
@@ -8,20 +8,31 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt", "r") as high_score:
+            self.high_score = int(high_score.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
         self.hideturtle()
         self.update_scoreboard()
+        
+    def reset_score(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as high_score:
+                high_score.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
 
     def update_scoreboard(self):
+        self.clear()
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    # The first version of the game finished when the snake hit the borders or hit it's tale
+    # def game_over(self):
+    #    self.goto(0, 0)
+    #    self.write("GAME OVER", align=ALIGNMENT, font=FONT)
